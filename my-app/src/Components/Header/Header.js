b9a7293bb0d5fb205c7dc637803a0c6c78182be4
KarRakib <rakibkazi2222@gmail.com>
2022-10-12 12:37:36 +0600

    icon and title change

--- my-app/src/Components/Header/Header.js
+++ my-app/src/Components/Header/Header.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='container d-flex justify-content-between navbar navbar-expand-lg bg-light'>
+        <nav className='container d-flex justify-content-between navbar navbar-expand-lg bg-light rounded p-2'>
             <h3>Web Development Quiz</h3>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
         </button>
             <div className='"collapse navbar-collapse" id="navbarSupportedContent"'>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
             <li class="nav-item"> <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link> </li>
             <li class="nav-item"> <Link className="nav-link active"  to={'/topics'}>Topics</Link> </li>
             <li class="nav-item"> <Link className="nav-link active"  to={'/statistics'}>Statistics</Link> </li>
